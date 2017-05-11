a39d3c9d711f375942a1c71e5cb932fccaeef983
Daniel Vega <danve4@gmail.com>
2017-05-11 18:51:33 -0500

    Added placeholder for client code functionality

--- site.js
+++ site.js
@@ -8,6 +8,18 @@ jQuery(function($) {
     });
 
     $('#code-form').on('submit', function (e) {
+      var code = ($('#code-text').val());//Gets text
+      console.log("You typed in: " + code);
+      getFileNames(code, function (data) {
+        $('#images').empty();
+        for(var i = 0; i < data.length; i++) {
+          $('#images').append(
+            '<li>'+
+            '<img src="../'+code+'/'+data[i]+'" alt="."/>' +
+            '</li>'
+          );
+        }
+      });
       e.preventDefault();
     });
 
@@ -30,5 +42,19 @@ jQuery(function($) {
       nav.addClass('visible');
       menuVisible = true;
     }
+  }
+
+  function getFileNames(path, callback) {
+    var query = '/'+path+'/contents.json';
+    console.log(query);
+    $.when($.ajax({
+      type: 'GET',
+      url: query
+    })).then(function(data) {
+      console.log(data);
+      callback(data);
+    }, function(e) {
+      console.log(e);
+    });
   }
 });
